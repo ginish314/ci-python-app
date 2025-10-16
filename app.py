d477ec46af523db0f8f0897f6aea5cbb138d5dfc
GINISH SAHU <ginishkumardurg@gmail.com>
2025-10-16 12:40:59 +0530

    Added pytest test file

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ def add(a, b):
     return a + b
 
 if __name__ == "__main__":
-    print("CI Pipeline सफलतापूर्वक चल रही है!")
+    print("CI Pipeline is running successfully!")
     print("2 + 3 =", add(2, 3))
+
